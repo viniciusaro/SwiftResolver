7c08fd81762e039bc2a21f09e82cfa395c9d18dc
Vinicius Alves Rodrigues <vinicius.rodrigues@CAMPUS-00236.local>
2017-10-03 10:51:44 -0300

    Add inject.stencil template
    This template allows the generation of extensions of types that
    have `anotated` properties with the sourcery comment `/// inject`.
    The generated extensions contains custom initializers with only
    the non injected dependencies, calling a default resolution
    method for the injected ones, where the dependency injection
    logic happens.

--- Scripts/swiftresolver.sh
+++ Scripts/swiftresolver.sh
@@ -2,7 +2,7 @@ OUTPUT="${1}"
 SOURCES_DIR="${PROJECT_DIR}"
 
 if [ -z $OUTPUT ]; then
-    OUTPUT="${PROJECT_DIR}/${PROJECT_NAME}/Resources/SwiftResolverGenerated.swift"
+    OUTPUT="${PROJECT_DIR}/${PROJECT_NAME}/Sources/SwiftResolver/SwiftResolverGenerated.swift"
 fi
 
-${PODS_ROOT}/Sourcery/bin/sourcery --templates "${PODS_ROOT}/SwiftResolver/Templates/container-register.stencil" --sources "${SOURCES_DIR}/" --output "${OUTPUT}"
+${PODS_ROOT}/Sourcery/bin/sourcery --templates "${PODS_ROOT}/SwiftResolver/Templates/inject.stencil" --sources "${SOURCES_DIR}/" --output "${OUTPUT}"
