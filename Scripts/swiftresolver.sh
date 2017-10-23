5fecb8b615d1e0f5d64fa8f2e6faa3afb1bfa85c
Vinicius Rodrigues <vinicius.a.ro@gmail.com>
2017-10-23 18:17:38 -0200

    [WIPPP]

--- Scripts/swiftresolver.sh
+++ Scripts/swiftresolver.sh
@@ -5,4 +5,5 @@ if [ -z $OUTPUT ]; then
     OUTPUT="${PROJECT_DIR}/${PROJECT_NAME}/Sources/SwiftResolver/SwiftResolverGenerated.swift"
 fi
 
-${PODS_ROOT}/Sourcery/bin/sourcery --templates "${PODS_ROOT}/SwiftResolver/Templates/inject.stencil" --sources "${SOURCES_DIR}/" --output "${OUTPUT}"
+#${PODS_ROOT}/Sourcery/bin/sourcery --templates "${PODS_ROOT}/SwiftResolver/Templates/inject.stencil" --sources "${SOURCES_DIR}/" --output "${OUTPUT}"
+${PODS_ROOT}/Sourcery/bin/sourcery --templates "/Users/vinicius.rodrigues/Documents/Development/SwiftResolver/Templates/container-register.stencil" --sources "${SOURCES_DIR}/" --output "${OUTPUT}"
